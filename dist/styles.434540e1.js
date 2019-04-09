3591675ca50cbc74f01914861f3870ce8771149b
shubhapradha <shubhapradha@angularcoursera.com>
2019-04-09 14:33:51 +0530

    latest build

--- dist/styles.434540e1.js
+++ dist/styles.434540e1.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64367" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61104" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
